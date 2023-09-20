79f47cbb082777a2eddea27871d9065f54f3909e
unknown <irene.sanchez@iesdoctorbalmis.com>
2023-09-20 21:18:13 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
+
+class Ui(ScreenManager):
+    pass
+
+class MainApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = 'Dark'
+        self.theme_cls.primary_palette = 'Teal'
+        Builder.load_file('design.kv')
+
+        return Ui()
+
+if __name__=="__main__":
+    MainApp().run()
